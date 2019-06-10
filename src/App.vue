023f8a65c30b1586651b5bb56599349f053a1ff5
Eli Filho <eli.filho@mv.com.br>
2019-06-10 14:23:31 -0300

    adição do grid.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,72 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col-2">
+        <label for="inline-form-input-name">Atividade</label>
+      </div>
+      <div class="col-8">
+        <b-input
+          id="inline-form-input-name"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          placeholder="Atividade a fazer..."
+          v-model="descritpion"
+        ></b-input>
+      </div>
+      <div class="col-2">
+        <b-button variant="primary"  v-on:click="adicionar()">Adicionar</b-button>
+      </div>
+    </div>
+    <br>
+    <br>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">Nº</th>
+          <th scope="col">Atividade</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  data: () => ({
+    atividade : '',
+    atividades: []
+  }),
+  methods:{
+    adicionar(){
+      this.atividades.push(atividade);
+    },
+
+    remover(){
+
+    },
+
+    alterar(){
+
+    }
+
   }
-}
+};
+
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
